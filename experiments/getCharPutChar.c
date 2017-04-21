71edbad6a4853c7997b2218ac90935f287d4ede3
Ratan Sunder Parai <ratanparai@gmail.com>
2017-04-21 06:19:28 +0600

    simple terminal echo program

---
+++ experiments/getCharPutChar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+main() {
+    int c;
+    c= getchar();
+    while(c != EOF) {
+        putchar(c);
+        c=getchar();
+    }
+    return EXIT_SUCCESS;
+}
